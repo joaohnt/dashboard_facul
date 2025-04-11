1502db22cf23d99fe786176cd426de67b71c4743
rafael-men <rafaelgoncalves1822@gmail.com>
2025-04-11 15:34:19 -0300

    secure dbConnection

--- dashboard_atosCapital/db/db_connection.py
+++ dashboard_atosCapital/db/db_connection.py
@@ -1,12 +1,16 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=aquidaba.infonet.com.br;'
-        'DATABASE=dbproinfo;'
-        'UID=leituraVendas;'
-        'PWD=KRphDP65BM;'
+        f"DRIVER={{{os.getenv('DB_DRIVER')}}};"
+        f"SERVER={os.getenv('DB_SERVER')};"
+        f"DATABASE={os.getenv('DB_NAME')};"
+        f"UID={os.getenv('DB_USER')};"
+        f"PWD={os.getenv('DB_PASSWORD')};"
     )
     return conn
 
