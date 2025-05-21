2e4fa0fbe7bc125307d60fd5e7aa0d086660c876
joaohnt <jpedrooo.cg@gmail.com>
2025-05-21 12:23:38 -0300

    ajeitando os graficos

--- dashboard_atosCapital/main_app.py
+++ dashboard_atosCapital/main_app.py
@@ -74,28 +74,34 @@ col2.metric("🎯 Média da Meta", f"{meta_media:.2f}%")
 col3.metric("📦 Quantidade de Vendas", f"{quantidade_vendas}")
 col4.metric("🧾 Ticket Médio", f"R$ {ticket_medio:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
-# Gráfico de vendas por mês
-st.subheader("📊 Vendas por Mês")
-df_mes = vendas_por_mes(df_filtered).to_pandas()
-fig_mes = px.bar(df_mes, x="mes", y="venda_total", title="Total de Vendas por Mês")
-st.plotly_chart(fig_mes, use_container_width=True)
 
-# Gráfico de vendas por semana
-st.subheader("📆 Vendas por Semana")
-df_semana = vendas_por_semana(df_filtered).to_pandas()
-fig_semana = px.line(df_semana, x="semana", y="venda_total", markers=True, title="Total de Vendas por Semana")
-st.plotly_chart(fig_semana, use_container_width=True)
+# Gráfico de vendas por mês - 2024
+st.subheader("📊 Vendas por Mês - 2024")
+df_2024 = (
+    df_filtered.with_columns(pl.col("dtVenda").dt.year().alias("Ano"))
+    .filter(pl.col("Ano") == 2024)
+    .group_by("Mes")
+    .agg(pl.col("vlVenda").sum().alias("venda_total"))
+    .sort("Mes")
+    .to_pandas()
+)
+fig_2024 = px.bar(df_2024, x="Mes", y="venda_total", title="Total de Vendas por Mês - 2024",
+                  labels={"Mes": "Mês", "venda_total": "Total Vendido (R$)"})
+st.plotly_chart(fig_2024, use_container_width=True)
 
-# Gráfico de Vendas por Filial
-st.subheader("🏢 Distribuição de Vendas por Filial")
-
-df_filial = df_filtered.group_by("nmFilial").agg(
-    pl.col("vlVenda").sum().alias("venda_total")
-).to_pandas()
-
-
-fig_filial = px.bar(df_filial, x="nmFilial", y="venda_total", title="Vendas Totais por Filial")
-st.plotly_chart(fig_filial, use_container_width=True)
+# Gráfico de vendas por mês - 2025
+st.subheader("📊 Vendas por Mês - 2025")
+df_2025 = (
+    df_filtered.with_columns(pl.col("dtVenda").dt.year().alias("Ano"))
+    .filter(pl.col("Ano") == 2025)
+    .group_by("Mes")
+    .agg(pl.col("vlVenda").sum().alias("venda_total"))
+    .sort("Mes")
+    .to_pandas()
+)
+fig_2025 = px.bar(df_2025, x="Mes", y="venda_total", title="Total de Vendas por Mês - 2025",
+                  labels={"Mes": "Mês", "venda_total": "Total Vendido (R$)"})
+st.plotly_chart(fig_2025, use_container_width=True)
 
 # Gráfico de pizza para cada filial
 st.subheader("📊 Distribuição de Vendas por Mês em Cada Filial")
@@ -114,43 +120,6 @@ fig_pizza_filial_mes = px.pie(df_filial_mes,
                                color_discrete_sequence=px.colors.qualitative.Set3)
 
 st.plotly_chart(fig_pizza_filial_mes, use_container_width=True)
-
-st.subheader("📅 Vendas por Dia da Semana em Cada Filial")
-
-# Adiciona a coluna com o dia da semana
-df_filtered = df_filtered.with_columns(
-    pl.col("dtVenda").dt.weekday().alias("dia_da_semana")
-)
-
-# Agrupa por filial e dia da semana e soma as vendas
-df_dia_semana_filial = df_filtered.group_by(["nmFilial", "dia_da_semana"]).agg(
-    pl.col("vlVenda").sum().alias("venda_total")
-).sort(["nmFilial", "dia_da_semana"])
-
-# Converte para pandas para usar com plotly
-df_dia_semana_filial = df_dia_semana_filial.to_pandas()
-
-# Mapeia o número do dia da semana para o nome do dia
-dias_da_semana = {
-    0: "Segunda-feira", 1: "Terça-feira", 2: "Quarta-feira", 3: "Quinta-feira", 
-    4: "Sexta-feira", 5: "Sábado", 6: "Domingo"
-}
-df_dia_semana_filial["dia_da_semana"] = df_dia_semana_filial["dia_da_semana"].map(dias_da_semana)
-
-# Gráfico de barras
-fig_dia_semana_filial = px.bar(
-    df_dia_semana_filial, 
-    x="dia_da_semana", 
-    y="venda_total", 
-    color="nmFilial", 
-    title="Vendas Totais por Dia da Semana em Cada Filial", 
-    labels={"dia_da_semana": "Dia da Semana", "venda_total": "Total de Vendas"},
-    color_discrete_sequence=px.colors.qualitative.Set3
-)
-
-st.plotly_chart(fig_dia_semana_filial, use_container_width=True)
-
-
 
 # Obter coordenadas das filiais filtradas
 filiais_coords = get_filiais_coordinates(selected_filiais)
